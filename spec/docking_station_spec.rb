f5e26b3d5e22e0b288043a19621ac63ac6e803ab
Tamara Jeffrey <just-tam@live.com>
2020-08-05 17:15:50 +0100

    raised no bikes errors

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,8 +5,9 @@ describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
   it "releases a bike" do
-    bike = subject.release_bike
-    expect(bike).to be_working
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.release_bike).to eq bike
   end
   it { is_expected.to respond_to(:bike) }
 
@@ -14,6 +15,12 @@ describe DockingStation do
     bike = Bike.new
     subject.dock(bike)
     expect(subject.bike).to eq bike
+  end
+
+  describe '#release_bike' do
+    it "raise an error when there are no bikes available" do
+      expect { subject.release_bike }.to raise_error "No bikes available"
+    end
   end
 
 end
