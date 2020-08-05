591aff51ab4829637256fab1099bf29c59028a6b
Tamara Jeffrey <just-tam@live.com>
2020-08-05 15:39:53 +0100

    added working method, created new bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,4 +4,13 @@ describe DockingStation do
 
   it { is_expected.to respond_to :release_bike }
 
+  it "returns a bike" do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
+
+  #it "bike is working" do
+  #  expect(subject.working?).to eq true
+  #end
+
 end
