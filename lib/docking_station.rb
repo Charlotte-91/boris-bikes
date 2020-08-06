7f18e0728d1e5f1d9008b99cf24f4c5cfdc4ccdb
Student <student@Makerss-MacBook-Air.local>
2020-08-06 14:57:34 +0100

    added ability to dock 20 bikes

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,15 +1,18 @@
 require_relative 'bike'
 
 class DockingStation
-attr_reader :bike
+attr_reader :bikes
+def initialize 
+  @bikes = []
+end
   def release_bike
-    fail "No bikes available" unless @bike
-    
-    @bike
+    fail "No bikes available" unless @bikes.empty?
+    @bikes.pop
   end
 
   def dock(bike)
-    fail "Docking station is full" if @bike
-    @bike = bike
+    fail "Docking station is full" if @bikes.length >= 20
+    @bikes << bike
+    @bikes[@bikes.length-1]
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -9,12 +9,11 @@ describe DockingStation do
     subject.dock(bike)
     expect(subject.release_bike).to eq bike
   end
-  it { is_expected.to respond_to(:bike) }
+  it { is_expected.to respond_to(:bikes) }
 
   it 'returns docked bikes' do
     bike = Bike.new
-    subject.dock(bike)
-    expect(subject.bike).to eq bike
+    expect(subject.dock(bike)).to eq bike
   end
 
   describe '#release_bike' do
@@ -24,7 +23,7 @@ describe DockingStation do
     end
   describe '#dock' do
     it "raise an error when the docking station is full" do
-      subject.dock(Bike.new)
+      20.times {subject.dock Bike.new}
       expect {subject.dock Bike.new }.to raise_error "Docking station is full"
      end
    end
